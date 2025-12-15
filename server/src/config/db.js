7b373c8603a9c0d1c8bfe159a4a6a5a835c0307d
RazaMuhammad5758 <waistband161@gmail.com>
2025-12-16 02:13:21 +0500

    frontend

--- server/src/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export async function connectDB() {
-  const uri = process.env.MONGO_URI;
-  if (!uri) throw new Error("MONGO_URI missing in .env");
-  await mongoose.connect(uri);
-  console.log("✅ MongoDB connected");
-}
